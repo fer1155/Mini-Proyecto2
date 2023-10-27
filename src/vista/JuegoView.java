612e80332f47c21f3053db0ea4404cbe83377710
Fernando Cardona <cardona.fernando@correounivalle.edu.co>
2023-10-27 01:24:42 -0500

    Solucionando error

--- src/vista/JuegoView.java
+++ src/vista/JuegoView.java
@@ -1,5 +1,51 @@
 package vista;
 
-public class JuegoView {
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
+import javax.swing.JPanel;
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
+
+public class JuegoView extends JFrame {
+    private JLayeredPane layeredPane;
+    private JPanel panel;
+    private ImageIcon imagenFondo;
+    
+    //Constructor de la ventana Juego
+    public JuegoView(){
+        this.setTitle("Adosados");
+        this.setBounds(200, 50, 900, 600);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+        iniciarComponentes();
+    }
+
+    private void iniciarComponentes() {
+        establecerLayeredPanel();
+        establecerPanel();
+        establecerFondo();
+    }
+    
+    private void establecerLayeredPanel() {
+        layeredPane = new JLayeredPane();
+        this.add(layeredPane);
+    }
+    
+    private void establecerPanel() {
+        panel = new JPanel();
+        panel.setBounds(0, 0, this.getWidth(), this.getHeight());
+        panel.setLayout(null);
+        layeredPane.add(panel, JLayeredPane.DEFAULT_LAYER);
+    }
+
+    private void establecerFondo() {
+        imagenFondo = new ImageIcon("");
+        JLabel etiquetaFondo = new JLabel();
+        etiquetaFondo.setBounds(0, 0, this.getWidth(), this.getHeight());
+        etiquetaFondo.setIcon(new ImageIcon(imagenFondo.getImage().getScaledInstance(etiquetaFondo.getWidth(), etiquetaFondo.getHeight(), Image.SCALE_SMOOTH)));
+        layeredPane.add(etiquetaFondo, JLayeredPane.PALETTE_LAYER);
+    }
     
 }
