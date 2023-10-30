4b3d5921e1ff78336f96d7be0282a1a679b8ce70
Oscar Munoz <omamr0304@gmail.com>
2023-10-30 14:22:29 -0500

    Ventana principal

--- src/vista/PrincipalView.java
+++ src/vista/PrincipalView.java
@@ -3,7 +3,9 @@ package vista;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Image;
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JLayeredPane;
@@ -15,6 +17,11 @@ public class PrincipalView extends JFrame {
     private JPanel panel;
     private ImageIcon imagenFondo;
     private JLabel titulo;
+    private ImageIcon imagenSoldador;
+    private JButton boton1;
+    private JButton boton2;
+    private JButton boton3;
+    
     
     //Constructor de la ventana Principal
     public PrincipalView(){
@@ -30,6 +37,10 @@ public class PrincipalView extends JFrame {
         establecerPanel();
         establecerFondo();
         establecerTitulo();
+        establecerSoldador();
+        establecerBoton1();
+        establecerBoton2();
+        establecerBoton3();
     }
     
     private void establecerLayeredPanel() {
@@ -52,14 +63,61 @@ public class PrincipalView extends JFrame {
         layeredPane.add(etiquetaFondo, JLayeredPane.PALETTE_LAYER);
     }
     
+     private void establecerSoldador() {
+        imagenSoldador = new ImageIcon("soldador.png");
+         JLabel etiquetaImg = new JLabel();
+         etiquetaImg.setBounds(0, 200, 360, 390);
+         etiquetaImg.setIcon(new ImageIcon(imagenSoldador.getImage().getScaledInstance(etiquetaImg.getWidth(), etiquetaImg.getHeight(), Image.SCALE_SMOOTH)));
+         layeredPane.add(etiquetaImg, JLayeredPane.MODAL_LAYER);
+    }  
+    
     private void establecerTitulo() {
         titulo = new JLabel("ADOSADOS");
-        titulo.setBounds(180, 20, 520, 100);
-        titulo.setOpaque(true); 
-        titulo.setBackground(Color.WHITE);
-        Color colorLetra = new Color(40, 28, 5);
+        titulo.setBounds(265, 20, 530, 120);
+        //titulo.setOpaque(true); 
+        //titulo.setBackground(Color.WHITE);
+        Color colorLetra = new Color(78, 39, 0);
         titulo.setForeground(colorLetra);
-        titulo.setFont(new Font("Verdana", 1, 80));
+        titulo.setFont(new Font("Bernard MT Condensed", 1, 85));
         layeredPane.add(titulo, JLayeredPane.MODAL_LAYER);    
+    }
+ 
+    private void establecerBoton1() {
+        boton1 = new JButton("JUGAR");
+        boton1.setBounds(455, 265, 180, 60); 
+        boton1.setForeground(Color.WHITE);
+        boton1.setFont(new Font("Bernard MT Condensed", 0, 28));
+        Color colorFondoBtn1 = new Color(78, 39, 0);
+        boton1.setBackground(colorFondoBtn1);
+        Color colorBorde2 = new Color(94, 94, 94);
+        boton1.setBorder(BorderFactory.createLineBorder(colorBorde2,3,true));
+        layeredPane.add(boton1, JLayeredPane.MODAL_LAYER);
+        boton1.setFocusPainted(false);
+    }
+    
+    private void establecerBoton2() {
+        boton2 = new JButton("¿COMO JUGAR?");
+        boton2.setBounds(310, 350, 230, 60); 
+        boton2.setForeground(Color.WHITE);
+        boton2.setFont(new Font("Bernard MT Condensed", 0, 28));
+        Color colorFondoBtn2 = new Color(78, 39, 0);
+        boton2.setBackground(colorFondoBtn2);
+        Color colorBorde2 = new Color(94, 94, 94);
+        boton2.setBorder(BorderFactory.createLineBorder(colorBorde2,3,true));
+        layeredPane.add(boton2, JLayeredPane.MODAL_LAYER);
+        boton2.setFocusPainted(false);
+    }
+    
+    private void establecerBoton3() {
+        boton3 = new JButton("¿PARA QUE SIRVE?");
+        boton3.setBounds(550, 350, 230, 60); 
+        boton3.setForeground(Color.WHITE);
+        boton3.setFont(new Font("Bernard MT Condensed", 0, 28));
+        Color colorFondoBtn3 = new Color(78, 39, 0);
+        boton3.setBackground(colorFondoBtn3);
+        Color colorBorde2 = new Color(94, 94, 94);
+        boton3.setBorder(BorderFactory.createLineBorder(colorBorde2,3,true));
+        layeredPane.add(boton3, JLayeredPane.MODAL_LAYER);
+        boton3.setFocusPainted(false);
     }
 }
