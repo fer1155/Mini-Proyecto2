4df26d839b7af1236c8ddc6f4bef3cd62152c714
Oscar Munoz <omamr0304@gmail.com>
2023-10-31 14:31:33 -0500

    Puse los creditos

--- src/miniproyecto2/Main.java
+++ src/miniproyecto2/Main.java
@@ -1,3 +1,12 @@
+/*
+Miniproyecto No. 2
+
+Fernando Cardona - 2241381
+Oscar Mario Muñoz - 2242481
+
+Grupo de FPOE: 80
+*/
+
 package miniproyecto2;
 
 import vista.PrincipalView;
--- src/modelo/PrincipalModel.java
+++ src/modelo/PrincipalModel.java
@@ -1,3 +1,12 @@
+/*
+Miniproyecto No. 2
+
+Fernando Cardona - 2241381
+Oscar Mario Muñoz - 2242481
+
+Grupo de FPOE: 80
+*/
+
 package modelo;
 
 public class PrincipalModel {
